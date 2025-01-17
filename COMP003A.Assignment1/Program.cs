44e76397de5458e44b2d52bb20f1a156cd460de5
TheGreatSkyline <156981030+TheGreatSkyline@users.noreply.github.com>
2025-01-16 16:55:09 -0800

    add writeline

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace COMP003A.Assignment1
+/*
+* Author: Joaquin Gomez
+* Course: COMP003A
+* Faculty: Jonathan Cruz
+
+namespace COMP003A.Assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
         }
     }
 }
